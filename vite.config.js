ce33f55cb839295a045fbd6e66d53edb4f239f16
Fabrizio <fabrinucci99@gmail.com>
2023-02-18 16:08:02 -0300

    Fix bug deploy

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://fabrinucci.github.io/",
+  base: "https://fabrinucci.github.io/gh-react",
   plugins: [react()],
 });
